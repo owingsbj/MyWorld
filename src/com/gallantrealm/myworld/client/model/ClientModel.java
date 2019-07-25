87e9c3272a00c76be2061b22feff28350ad402b5
BJ Owings <bj@gallantrealm.com>
2019-07-25 14:51:09 -0700

    Add getGallantUrl

--- src/com/gallantrealm/myworld/client/model/ClientModel.java
+++ src/com/gallantrealm/myworld/client/model/ClientModel.java
@@ -1107,6 +1107,14 @@ public abstract class ClientModel {
 		properties.put(propertyName, value);
 	}
 
+	public String getGallantUrl() {
+		if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+			return "https://gallantrealm.com";
+		} else {
+			return "http://gallantrealm.com";
+		}
+	}
+
 	// Private methods
 
 }
