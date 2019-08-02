6b29712e080ebd9609069c6bf95980ff136d525c
BJ Owings <bj@gallantrealm.com>
2019-08-01 17:40:56 -0700

    fix ontouch bug

--- src/com/gallantrealm/myworld/android/ShowWorldActivity.java
+++ src/com/gallantrealm/myworld/android/ShowWorldActivity.java
@@ -979,10 +979,8 @@ public class ShowWorldActivity extends Activity implements OnTouchListener, Clie
 	/**
 	 * All forms of avatar control funnel into this method.
 	 * 
-	 * @param deltaX
-	 *            the movement in the x dimension, from -50 to 50
-	 * @param deltaY
-	 *            the movement in the y dimension, from -50 to 50
+	 * @param deltaX the movement in the x dimension, from -50 to 50
+	 * @param deltaY the movement in the y dimension, from -50 to 50
 	 */
 	public void controller(float deltaX, float deltaY) {
 		updateJoyThumb((int) deltaX, (int) deltaY);
@@ -1056,26 +1054,24 @@ public class ShowWorldActivity extends Activity implements OnTouchListener, Clie
 	/**
 	 * Determines if given points are inside view
 	 * 
-	 * @param x
-	 *            - x coordinate of point
-	 * @param y
-	 *            - y coordinate of point
-	 * @param view
-	 *            - view object to compare
+	 * @param x    - x coordinate of point
+	 * @param y    - y coordinate of point
+	 * @param view - view object to compare
 	 * @return true if the points are within view bounds, false otherwise
 	 */
 	private boolean isPointInsideView(float x, float y, View view) {
-		int location[] = new int[2];
-		view.getLocationOnScreen(location);
-		int viewX = location[0];
-		int viewY = location[1];
+		if (view.getVisibility() == View.VISIBLE) {
+			int location[] = new int[2];
+			view.getLocationOnScreen(location);
+			int viewX = location[0];
+			int viewY = location[1];
 
-		// point is inside view bounds
-		if ((x > viewX && x < (viewX + view.getWidth())) && (y > viewY && y < (viewY + view.getHeight()))) {
-			return true;
-		} else {
-			return false;
+			// point is inside view bounds
+			if ((x > viewX && x < (viewX + view.getWidth())) && (y > viewY && y < (viewY + view.getHeight()))) {
+				return true;
+			}
 		}
+		return false;
 	}
 
 	private float getViewX(float x, View view) {
